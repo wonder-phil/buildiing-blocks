2b556274284f9e78560cda0b5827dbc69b63813c
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2018-11-12 07:55:05 -0500

    Updated the mining function
    Should use the Java thread shared memory model or messaging

--- mqtt/MineBlock.java
+++ mqtt/MineBlock.java
@@ -1,23 +1,39 @@
 package b;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import com.google.gson.Gson;
+
 public class MineBlock implements Runnable {
 
-	private volatile boolean exit = false;
-	
-	@Override
+    private volatile boolean exit = false;
+    private String fileNPath = "block.json";
+    
+    
+    @Override
 	public void run() {
-		while(!exit) {
-			for (int i =0 ; i < 10000000; i++)  {
-				int j = 2;
-				j += 1;
-			}
-			System.out.println("Running");
-		}
-		System.out.println("Stopped");
-	}
 	
-	public void stop(){
-		exit = true;
+	
+	BufferedReader bufferedReader = null;
+	try {
+	    bufferedReader = new BufferedReader(new FileReader(fileNPath));
+	} catch (FileNotFoundException e) {
+	    System.err.println(" Can't read JSON Block ");
+	    e.printStackTrace();
 	}
+
+        Gson gson = new Gson();
+        Block json = gson.fromJson(bufferedReader, Block.class); // Block.class
+        
+        System.out.println("Mine Block");
+        System.out.println(json.toString());
+	
+	
+    }
+    
+    public void stop(){
+	exit = true;
+    }
 
 }
