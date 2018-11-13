6fab39c2a9ede1b6038825e3de05a1724fd3119e
Phillip G. Bradford <phillip.g.bradford@gmail.com>
2018-11-13 15:24:37 -0500

    Minor cleanup - needs a lot more

--- mqtt/simple/SubscriberCallback.java
+++ mqtt/simple/SubscriberCallback.java
@@ -40,70 +40,70 @@ public class SubscriberCallback implements MqttCallback {
     
     @Override
 	public void connectionLost(Throwable arg0) {
-	System.out.println("Connection to MQTT broker lost!");
+    		System.out.println("Connection to MQTT broker lost!");
     }
 
     @Override
 	public void deliveryComplete(IMqttDeliveryToken arg0) {
 	
-	String[] topics = arg0.getTopics();
-	for (String t : topics) {
-	    System.out.println(t);
-	}
+		String[] topics = arg0.getTopics();
+		for (String t : topics) {
+		    System.out.println(t);
+		}
     }
 
     @Override
 	public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
 	
-	System.out.println("Message received:\t"+ new String(mqttMessage.getPayload()) );
-	System.out.println("mqttMessage topic: " + topic);
-	
-	
-	System.out.println("Message received !:\t"+ new String(mqttMessage.getPayload()) );
-	    
-	String value = new String(mqttMessage.getPayload());
-	    
-	Block b = new Block("Foo", "Bar");
-	
-	SharedData.difficulty = 3;
-	
-	    
-	    
-	switch(topic) {
-	        
-	case "last_block":
-	    block = gson.fromJson(value, Block.class);
-	    System.out.println(value);
-	    
-	    SharedData.lastBlock = value;
-	    
-	    
-	    
-	    break;
-	        
-	case "mine_state":
-	    if (value.equals(new String("START"))) {
+		System.out.println("Message received:\t"+ new String(mqttMessage.getPayload()) );
+		System.out.println("mqttMessage topic: " + topic);
 		
-		System.out.println("--->YES");
 		
-		if (!thread.isAlive()) {
-		    thread.start();
+		System.out.println("Message received !:\t"+ new String(mqttMessage.getPayload()) );
+		    
+		String value = new String(mqttMessage.getPayload());
+		    
+		Block b = new Block("Foo", "Bar");
+		
+		SharedData.difficulty = 3;
+		
+		    
+		    
+		switch(topic) {
+		        
+		case "last_block":
+		    block = gson.fromJson(value, Block.class);
+		    System.out.println(value);
+		    
+		    SharedData.lastBlock = value;
+		    
+		    
+		    
+		    break;
+		        
+		case "mine_state":
+		    if (value.equals(new String("START"))) {
+			
+			System.out.println("--->YES");
+			
+			if (!thread.isAlive()) {
+			    thread.start();
+			}
+			
+			
+		    } else { // STOP
+			
+			System.out.println("--->No");
+			mineBlock.stop();
+			thread.interrupt();
+		    }
+		    
+		    break;
+		        
+		default:
+		    System.out.println("DEFAULT");
+		    
 		}
-		
-		
-	    } else { // STOP
-		
-		System.out.println("--->No");
-		mineBlock.stop();
-		thread.interrupt();
-	    }
-	    
-	    break;
-	        
-	default:
-	    System.out.println("DEFAULT");
-	    
-	}
     }
 
 }
